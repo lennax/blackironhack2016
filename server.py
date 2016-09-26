ab6d91bc65c2871783c9c9b2920b59064921f6bf
Lenna Peterson <arklenna@gmail.com>
2016-09-26 17:07:55 -0400

    Add rough mosquito cutoff

--- server.py
+++ server.py
@@ -104,47 +104,60 @@ def calculate():
 #       Value12 is the December value.
 #       Flag12  is the completeness flag for December.
 
+    def get_row(filename):
+        row_list = list()
+        ftp.retrlines("RETR {0}".format(filename), row_list.append)
+        for line in row_list:
+            stnid = line[:11]
+            if stnid == stationid:
+                return line
 
     #ftp.retrlines("RETR products/auxiliary/station/{0}-normals.txt".format(stationid))
     
-    # Cooling degree days are equivalent to growing degree days
-    cooling_list = list()
-    ftp.retrlines("RETR products/temperature/mly-cldd-base57.txt", cooling_list.append)
+    # http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4001452/
+    # Aedes albopictus is not expected to survive average January temperatures of -5 C (23 F)
+#    tenths of degrees Fahrenheit for maximum, minimum, average, dew point, heat 
+#    index, wind chill, and air temperature normals and standard deviations. 
+#    e.g., "703" is 70.3F
+    tavg_row = get_row("products/temperature/mly-tavg-normal.txt")
+    jan_temp = int(tavg_row.split()[1][:-1]) * 0.1
+    temp_risk = bool(jan_temp > 23.0)
     
-    cooling_result = None
-    for line in cooling_list:
-        stnid = line[0:11]
-        #print stationid, stnid, stnid == stationid
-        if stnid == stationid:
-            cooling_result = line
-#    print cooling_result
-    cooling_list = cooling_result.split()
-    cooling_value = int(cooling_list[month_number][:-1])
+    # Aedes albopictus requires a minimum annual rainfall of ~250 mm (9.8 inches)
+#    tenths of inches for average monthly/seasonal/annual snowfall, 
+#    month-to-date/year-to-date snowfall, and percentiles of snowfall. 
+#    e.g. "39" is 3.9"
+    rain_row = get_row("products/precipitation/ann-prcp-normal.txt")
+    rain_in = int(rain_row.split()[1][:-1]) * 0.1
+    rain_risk = bool(rain_in >= 9.8)
     
-#    hundredths of inches for average monthly/seasonal/annual precipitation, 
-#    month-to-date/year-to-date precipitation, and percentiles of precipitation. 
-#    e.g., "1" is 0.01" and "1486" is 14.86"
+    if temp_risk and rain_risk:
+    
+        # Cooling degree days are equivalent to growing degree days
+        cooling_result = get_row("products/temperature/mly-cldd-base57.txt")
+    #    print cooling_result
+        cooling_list = cooling_result.split()
+        cooling_value = int(cooling_list[month_number][:-1])
 
-    precip_list = list()
-    ftp.retrlines("RETR products/precipitation/mly-prcp-normal.txt", precip_list.append)
-    
-    precip_result = None
-    for line in precip_list:
-        stnid = line[0:11]
-        #print stationid, stnid, stnid == stationid
-        if stnid == stationid:
-            precip_result = line
-#    print precip_result
-    precip_list = precip_result.split()
-    precip_value = int(precip_list[month_number][:-1])
-    
-    result_text = "At your destination, {month_name} normally has {0} cooling degree days and {1} inches of rain".format(cooling_value, precip_value * 0.01, month_name=month_name)
-    
+    #    hundredths of inches for average monthly/seasonal/annual precipitation, 
+    #    month-to-date/year-to-date precipitation, and percentiles of precipitation. 
+    #    e.g., "1" is 0.01" and "1486" is 14.86"
+        precip_result = get_row("products/precipitation/mly-prcp-normal.txt")
+    #    print precip_result
+        precip_list = precip_result.split()
+        precip_value = int(precip_list[month_number][:-1]) * 0.01
+        
     # TODO determine amount of degree days and precipitation needed for mosquitoes
-    
+        
+        result_text = "The climate at your destination is hospitable to mosquitoes. {month_name} normally has {0} cooling degree days and {1} inches of rain.".format(cooling_value, precip_value, month_name=month_name)
+        
     # TODO compute some sort of risk
-    risk = 15
+        risk = 15
     
+    else:
+        result_text = "The climate at your destination is not hospitable to mosquitoes."
+        risk = 1
+
     result_dict = dict(text=result_text,
                        risk=risk)
     
