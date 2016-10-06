d92cb71d3dabc3d1f017b417507910129e96288f
Lenna Peterson <arklenna@gmail.com>
2016-10-05 20:42:11 -0400

    Add destination autocomplete

--- static/script.js
+++ static/script.js
@@ -47,9 +47,61 @@ MyApp.initMap = function () {
     //            panControl: false,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  // Not sure if this needs to be declared outside
   MyApp.map = new google.maps.Map(mapCanvas, mapOptions);
 
+  var map = MyApp.map;
+
+  var input = document.getElementById('destination');
+
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener('place_changed', function () {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17); // Why 17? Because it looks good.
+    }
+    marker.setIcon( /** @type {google.maps.Icon} */ ({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
 };
 
 MyApp.drawGauge = function (risk) {
@@ -80,7 +132,7 @@ MyApp.drawGauge = function (risk) {
 
 MyApp.drawLadder = function (destcases, incases) {
   "use strict";
-  
+
   var cause = [
 'Heart disease',
 'Lung cancer',
@@ -171,19 +223,19 @@ MyApp.drawLadder = function (destcases, incases) {
     marker: deathMarker
   };
 
-//  var gbsCases = 15;
-//  var gbstrace = {
-//    type: 'scatter',
-//    x: [gbsCases],
-//    y: [gbsCases],
-//    //y: ['Guillain-Barré Syndrome'],
-//    text: ['Guillain-Barré Syndrome'],
-//    textposition: 'center',
-//    mode: 'markers',
-//    name: 'Cases per 1M in USA',
-//    //showlegend: false,
-//    marker: caseMarker
-//  };
+  //  var gbsCases = 15;
+  //  var gbstrace = {
+  //    type: 'scatter',
+  //    x: [gbsCases],
+  //    y: [gbsCases],
+  //    //y: ['Guillain-Barré Syndrome'],
+  //    text: ['Guillain-Barré Syndrome'],
+  //    textposition: 'center',
+  //    mode: 'markers',
+  //    name: 'Cases per 1M in USA',
+  //    //showlegend: false,
+  //    marker: caseMarker
+  //  };
 
   var zikatrace = {
     type: 'scatter',
@@ -196,7 +248,7 @@ MyApp.drawLadder = function (destcases, incases) {
     //showlegend: false,
     marker: caseMarker
   };
-  
+
   var zikatrace_in = {
     type: 'scatter',
     x: [incases],
@@ -209,7 +261,7 @@ MyApp.drawLadder = function (destcases, incases) {
     marker: caseMarkerIn
   };
 
-  var data = [smalltrace, largetrace, 
+  var data = [smalltrace, largetrace,
 //              gbstrace,
               zikatrace,
               zikatrace_in,
@@ -288,9 +340,9 @@ MyApp.drawLadder = function (destcases, incases) {
         xref: 'x',
         yref: 'y',
         text: 'Zika Virus Syndrome (IN)',
-//        font: {
-//          color: caseColor,
-//        },
+        //        font: {
+        //          color: caseColor,
+        //        },
         bgcolor: 'rgba(255, 255, 255, 0.8)',
         bordercolor: caseColorIn,
         borderwidth: 2,
@@ -307,9 +359,9 @@ MyApp.drawLadder = function (destcases, incases) {
         xref: 'x',
         yref: 'y',
         text: 'Zika Virus Syndrome',
-//        font: {
-//          color: caseColor,
-//        },
+        //        font: {
+        //          color: caseColor,
+        //        },
         bgcolor: 'rgba(255, 255, 255, 0.8)',
         bordercolor: caseColor,
         borderwidth: 2,
@@ -354,7 +406,7 @@ MyApp.submitForm = function () {
   "use strict";
 
   if (MyApp.validData()) {
-    
+
     // Clear result div
     $('#result').text("");
     $('#ladder').text("");
@@ -467,17 +519,17 @@ $(document).ready(function () {
   $('input#submit').bind('click', MyApp.submitForm);
 
   // Loading animation code
-  $( document ).ajaxStart(function() {
+  $(document).ajaxStart(function () {
     MyApp.disableSubmit();
     $('body').addClass("loading");
     //console.log( "Triggered ajaxStart handler." );
   });
-  $( document ).ajaxStop(function() {
+  $(document).ajaxStop(function () {
     $('body').removeClass("loading");
     MyApp.checkSubmit();
     //console.log( "Triggered ajaxStop handler." );
   });
-  
+
   // Bind enter to submit
   $('input[type=text]').bind('keydown', function (e) {
     if (e.keyCode == 13) {
